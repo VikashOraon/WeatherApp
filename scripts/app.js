46453243931c9c58d9a07d1e32173cbbcfdd2c8c
VikashOraon <72189417+VikashOraon@users.noreply.github.com>
2020-10-03 15:47:35 +0530

    Added comments

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,18 @@
- const cityForm = document.querySelector('form');
+//Getting refrence of elements
+const cityForm = document.querySelector('form');
  const card = document.querySelector('.card');
  const details = document.querySelector('.details');
  const time = document.querySelector('img.time');
  const icon = document.querySelector('.icon img');  
 
- const updateUI = (data) =>{
+//DOM manipulation
+const updateUI = (data) =>{
 
     // const cityDetail = data.cityDetail;
     // const weather = data.weather;
+    //the below code is equivalent to the above comment(Object Shorthand Notation)
      const { cityDetail , weather } = data;
-    
+    //update detail template
     details.innerHTML =`
     <h5 class="my-3">${cityDetail.EnglishName}</h5>
     <div class="my-3">${weather.WeatherText}</div>
@@ -29,6 +32,7 @@
     }
     time.setAttribute('src', timeSrc);   
     
+ //remove d-none class if exist
     if(card.classList.contains('d-none')){
         card.classList.remove('d-none');
     }
@@ -47,11 +51,14 @@
 
  cityForm.addEventListener('submit', (e)=>{
 
+    //prvent deafault browser action 
     e.preventDefault();
     
+  //get City value
     const city = cityForm.city.value.trim();
     cityForm.reset();
-
+    
+  //update the UI with new city
     updateCity(city)
     .then(data => updateUI(data))
     .catch(err => console.log(err));
